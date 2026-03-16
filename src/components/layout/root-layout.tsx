ee42b571d6c18a293328baf6c660a110d1b4d04b
tuanvumaihuynh <tuanvux01@gmail.com>
2026-03-16 23:00:03 +0700

    fix empty page

--- src/components/layout/root-layout.tsx
+++ src/components/layout/root-layout.tsx
@@ -1,23 +1,15 @@
-import { Outlet, useLocation } from 'react-router-dom'
-import { AnimatePresence } from 'framer-motion'
+import { Outlet } from 'react-router-dom'
 import { Toaster } from '@/components/ui/sonner'
 import { Header } from './header'
 import { Footer } from './footer'
 import { ScrollToTop } from './scroll-to-top'
-import { PageTransition } from './page-transition'
 
 export function RootLayout() {
-  const location = useLocation()
-
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
       <main className="flex-1">
-        <AnimatePresence mode="wait">
-          <PageTransition key={location.pathname}>
-            <Outlet />
-          </PageTransition>
-        </AnimatePresence>
+        <Outlet />
       </main>
       <Footer />
       <ScrollToTop />
